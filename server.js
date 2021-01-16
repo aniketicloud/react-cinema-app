c95478ec9ac4aa7e3681ef2bf06c803508caccb4
Aniket Pandharabale <aniketicloud@gmail.com>
2021-01-16 14:11:37 +0530

    feature: deploy to Heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// change build as per framework
+// e.g. vue uses build, angular uses dist
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`React app running on port ${PORT}`);
+})
