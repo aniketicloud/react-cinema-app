1648427808f1e929bbb25232b51740f75920f511
Aniket Pandharabale <aniketicloud@gmail.com>
2021-07-12 17:17:34 +0530

    revert branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
 
 function App() {
+  console.log(moment);
   return (
     <div className="App">
       <header className="App-header">
+        {moment('20011104T120854').format('MMMM Do YYYY')}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
